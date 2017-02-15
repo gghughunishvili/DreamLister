e3e42e27da7905321d1419f354d58c22d543d470
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-02-15 17:23:49 +0400

    Made few changes, added reusable cell and basic skeleton of app

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Giorgi Ghughunishvili on 2/15/17.
+//  Copyright © 2017 Giorgi Ghughunishvili. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+}
---
+++ DreamLister/MainVC.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  DreamLister
+//
+//  Created by Giorgi Ghughunishvili on 1/24/17.
+//  Copyright © 2017 Giorgi Ghughunishvili. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class MainVC: UIViewController,UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segment: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+
+}
+
