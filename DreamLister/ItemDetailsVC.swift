59f1bdd8f0d9e2cab5ce326c85ba87a3a74099e4
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-02-23 00:00:52 +0400

    Added item saving implementation

--- DreamLister/ItemDetailsVC.swift
+++ DreamLister/ItemDetailsVC.swift
@@ -26,23 +26,23 @@ class ItemDetailsVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSou
         storePicker.delegate = self
         storePicker.dataSource = self
         
-        let store1 = Store(context: context)
-        store1.name = "Jason Inc"
-        
-        let store2 = Store(context: context)
-        store2.name = "Buy If you can"
-        
-        let store3 = Store(context: context)
-        store3.name = "Best Buy"
-        
-        let store4 = Store(context: context)
-        store4.name = "No Mercy"
-        
-        let store5 = Store(context: context)
-        store5.name = "Why Not?!"
-        
-        let store6 = Store(context: context)
-        store6.name = "True Value"
+//        let store1 = Store(context: context)
+//        store1.name = "Jason Inc"
+//        
+//        let store2 = Store(context: context)
+//        store2.name = "Buy If you can"
+//        
+//        let store3 = Store(context: context)
+//        store3.name = "Best Buy"
+//        
+//        let store4 = Store(context: context)
+//        store4.name = "No Mercy"
+//        
+//        let store5 = Store(context: context)
+//        store5.name = "Why Not?!"
+//        
+//        let store6 = Store(context: context)
+//        store6.name = "True Value"
         
         ad.saveContext()
         
@@ -50,6 +50,27 @@ class ItemDetailsVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSou
         
     }
     
+    @IBAction func savePressed(_ sender: UIButton) {
+        let item = Item(context: context)
+        
+        if let title = titleField.text {
+            item.title = title
+        }
+        
+        if let price = priceField.text {
+            item.price = (price as NSString).doubleValue
+        }
+        
+        if let details = detailsField.text {
+            item.details = details
+        }
+        
+        item.toStore = stores[storePicker.selectedRow(inComponent: 0)]
+        
+        ad.saveContext()
+        
+        _ = navigationController?.popViewController(animated: true)
+    }
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         let store = stores[row]
         return store.name
