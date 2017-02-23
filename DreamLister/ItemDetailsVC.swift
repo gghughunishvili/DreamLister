6d4656b5bafa73fae94277402cba7ad8dc5602ac
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-02-23 13:22:18 +0400

    Fixed the issue when user enters in the text field and can’t dismiss the keyboard

--- DreamLister/ItemDetailsVC.swift
+++ DreamLister/ItemDetailsVC.swift
@@ -26,6 +26,14 @@ class ItemDetailsVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSou
         storePicker.delegate = self
         storePicker.dataSource = self
         
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ItemDetailsVC.dismissKeyboard))
+        
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+        
+        view.addGestureRecognizer(tap)
+        
         ad.saveContext()
         
         //emptyStores()
@@ -116,6 +124,12 @@ class ItemDetailsVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSou
             // Catch the error
         }
         
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
     
     
